d9c31ab4196d5c6e03de418fc92354c523ba8720
Mohammad-Taizul-Islam <m.taizul1045@gmail.com>
2025-01-20 06:13:44 +0600

    portal-switcher

---
+++ portal/portal-example/src/Components/PopupContent.jsx
@@ -0,0 +1,23 @@
+
+// eslint-disable-next-line react/prop-types
+
+import { createPortal } from "react-dom";
+const PopupContent = ({ copied }) => {
+  return createPortal(
+    <section>
+      { copied && (
+        <div
+          style={{
+            position: "absolute",
+            bottom: "3rem",
+          }}
+        >
+          Copied to Clipboard
+        </div>
+      )}
+    </section>,
+    document.querySelector("#popup-content")
+  );
+};
+
+export default PopupContent;
---
+++ portal/switcher/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Switcher from './Switcher'
+
+const App = () => {
+  return (
+    <div>
+      <Switcher/>
+    </div>
+  )
+}
+
+export default App
